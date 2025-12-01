cf30c71f3d267e3c514862ad4c65872627af7eba
abhayarora01 <aroraabhay390@gmail.com>
2025-12-01 15:18:12 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,1 +1,23 @@
-hi
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+require('dotenv').config();
+const connectDB = require('./config/db');
+const router = require('./routes');
+
+const app = express();
+
+app.use(cors({
+    origin: true,
+    credentials: true,
+}));
+
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+app.use(cookieParser());
+
+app.use("/api", router);
+
+
+
+connectDB();
