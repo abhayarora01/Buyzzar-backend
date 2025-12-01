66cffece6de28138087db7caa645249235007e56
abhayarora01 <aroraabhay390@gmail.com>
2025-12-01 15:50:13 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -16,7 +16,7 @@ app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(cookieParser());
 
-app.use("/api", router);
+app.use(router);
 
 
 
